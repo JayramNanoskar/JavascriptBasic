589f2bbdb7beb34f5673e403249b03fabeaf8567
JayramNanoskar <jayram.n978@gmail.com>
2020-09-27 18:16:00 +0530

    adding arrow function callback

--- demo3.js
+++ demo3.js
@@ -8,3 +8,10 @@ console.log(checkEven(9));
 var result = [2, 4, 6, 8, 24].every(checkEven);
 
 console.log(result);
+
+var res = [2, 3, 6, 8, 24].every((e) => {
+    return e % 2 === 0;
+});
+
+console.log(res);
+
