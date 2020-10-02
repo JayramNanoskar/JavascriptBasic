d9660d3f60be02f6c5104da288c9a543be14e48d
JayramNanoskar <jayram.n978@gmail.com>
2020-10-02 21:41:08 +0530

    using proto | to inject function in existing object's prototype

--- demo9.js
+++ demo9.js
@@ -3,8 +3,14 @@ var User = function(name, hobbiesCount){
     this.hobbiesCount = hobbiesCount 
 }
 
+User.prototype.getDetails = function getDetail(){
+    console.log(`${this.name} has ${this.hobbiesCount} hobbies `);
+};
+
 var john = new User('john', 2);
 console.log(john);
+john.getDetails();
 
 var vishal = new User('vishal', 3);
 console.log(vishal);
+vishal.getDetails();
