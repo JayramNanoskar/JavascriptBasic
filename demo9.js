87e49f766b70c693f97c34424272a6c0ae5eab04
JayramNanoskar <jayram.n978@gmail.com>
2020-10-02 21:21:19 +0530

    using new operator | to create an instance

---
+++ demo9.js
@@ -0,0 +1,10 @@
+var User = function(name, hobbiesCount){
+    this.name = name,
+    this.hobbiesCount = hobbiesCount 
+}
+
+var john = new User('john', 2);
+console.log(john);
+
+var vishal = new User('vishal', 3);
+console.log(vishal);
