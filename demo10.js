afdf5161a02891a8f717878a5367c30423094c66
JayramNanoskar <jayram.n978@gmail.com>
2020-10-03 14:44:50 +0530

    injecting properties/functions in existing object's prototype | in case of using Object.create() to create an instance

--- demo10.js
+++ demo10.js
@@ -2,10 +2,19 @@ var User = {
     name : "",
     getUserName : function(){
         console.log(`User name is ${this.name}`);
-    },
+    }
 };
 
 var vijay = Object.create(User);
 vijay.name = "Vijay";
 console.log(vijay);
 vijay.getUserName();
+
+var manish = Object.create(User, {
+    name : {value : "Manish"},
+    hobbiesCount : {value : 3},
+});
+
+console.log(manish);
+console.log(manish.hobbiesCount);
+manish.getUserName();
