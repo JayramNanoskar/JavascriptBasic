35afab789e0f85fbca73cd5e0a211850699ad024
JayramNanoskar <jayram.n978@gmail.com>
2020-10-04 22:16:10 +0530

    using Promise, async and await in js

--- demo20.js
+++ demo20.js
@@ -3,21 +3,30 @@ const one = () => {
 }
 
 const two = () => {
-    setTimeout(() => {
-        console.log(new Date());
-    }, 10);
-    return "I am second";
-}
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("I am second");
+        }, 1000);
+    });
+};
 
 const three = () => {
     return "I am third";
 }
 
 
-const callMe = () => {
-    console.log(two());
-    console.log(three());
-    console.log(one());
+const callMe = async () => {
+    let valOne = one();
+    console.log(valOne);
+    let valTwo = await two();
+    console.log(valTwo);
+    let valThree = three();
+    console.log(valThree);
 };
 
+// Now with Promise, async and await - output will be
+//I am first
+//I am second
+//I am third
+//Infact execution will block/wait till two() complete then take three() which was not case prior Promise, async and await use
 callMe();
