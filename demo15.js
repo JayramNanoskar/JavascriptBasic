907eda65f5023b0906e120d5d93c07aaf1c9b6ce
JayramNanoskar <jayram.n978@gmail.com>
2020-10-04 13:43:10 +0530

    using template literals in js

--- demo15.js
+++ demo15.js
@@ -4,4 +4,11 @@ if(true){
 }
 
 console.log(firstName); //john - treat as global scope
-console.log(lastName); //ReferenceError: lastName is not defined  - treat as local scope
+//console.log(lastName); //ReferenceError: lastName is not defined  - treat as local scope
+
+var progLangName = "Javascript";
+var feedback = `${progLangName} is a lightweight, interpreted programming language `;
+console.log(feedback.charAt(2));
+console.log(feedback.endsWith("y"));
+console.log(feedback.includes("script"));
+console.log(feedback.toUpperCase());
